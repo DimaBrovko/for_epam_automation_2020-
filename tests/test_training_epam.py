11709cdbf086f72efb7fa60cc438f3e1f549c32e
DimaBrovko <halixfax@gmail.com>
2020-05-12 20:00:03 +0300

    add test - login to account

--- tests/test_training_epam.py
+++ tests/test_training_epam.py
@@ -1,7 +1,16 @@
-from selene import browser
+from selene.support.shared import browser, config
+from selene import by, have
 
-def test_one():
-    browser.open_url("https://www.training.epam.ua/");
+def test_login():
+    browser.open_url("https://www.training.epam.ua/")
+
+    browser.element('//div[@class="header-control header-auth"]').click()
+
+    browser.element('//input[@id="signInEmail"]').type('test2020.epam@gmail.com')
+    browser.element('//input[@id="signInPassword"]').type('Qwe123ewQ')
 
 
-test_one();
+    browser.element('//input[contains(@class,"popup-reg-sign-in-form__sign-in")]').click()
+
+
+test_login()
